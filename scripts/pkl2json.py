225391cf285f6d8fc6e58d16da0f4b8086473cc2
marekstrelec <marek.strelec@gmail.com>
2016-12-01 01:02:24 +0000

    dumping scores for histogram now

--- scripts/pkl2json.py
+++ scripts/pkl2json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-import cPickle
+import _pickle as cPickle
 import json
 import operator
 
--- src/cpu/mblas/matrix.cpp
+++ src/cpu/mblas/matrix.cpp
@@ -12,143 +12,161 @@
 
 #include "cpu/decoder/encoder_decoder.h"
 
+// histogram
+#include "common/vocab.h"
+#include <fstream>
+
+
 using namespace std;
 
 namespace CPU {
 
-namespace mblas {
+    namespace mblas {
 
 /////////////////////////////////////////////////////////////////////
-struct ProbCompare {
-  ProbCompare(const float* data) : data_(data) {}
+        struct ProbCompare {
+            ProbCompare(const float* data) : data_(data) {}
 
-  bool operator()(const unsigned a, const unsigned b) {
-    return data_[a] > data_[b];
-  }
+            bool operator()(const unsigned a, const unsigned b) {
+                return data_[a] > data_[b];
+            }
 
-  const float* data_;
-};
+            const float* data_;
+        };
+
+        ofstream out_hist;
 
 /////////////////////////////////////////////////////////////////////
-void ArrayMatrix::BestHyps(Beam& bestHyps,
-    const Beam& prevHyps,
-		BaseMatrices& ProbsEnsemble,
-		const size_t beamSize,
-		History& history,
-		const std::vector<ScorerPtr> &scorers,
-		const Words &filterIndices,
-    bool returnAlignment) const
-{
-  using namespace mblas;
+        void ArrayMatrix::BestHyps(Beam& bestHyps,
+                                   const Beam& prevHyps,
+                                   BaseMatrices& ProbsEnsemble,
+                                   const size_t beamSize,
+                                   History& history,
+                                   const std::vector<ScorerPtr> &scorers,
+                                   const Words &filterIndices,
+                                   bool returnAlignment) const
+        {
+            using namespace mblas;
 
-  auto& weights = God::GetScorerWeights();
+            // open file for appending scores to the end of the file
+            out_hist.open("histogram.out", ios::app);
 
-  mblas::ArrayMatrix& Probs = static_cast<mblas::ArrayMatrix&>(*ProbsEnsemble[0]);
+            auto& weights = God::GetScorerWeights();
 
-  mblas::ArrayMatrix Costs(Probs.rows(), 1);
-  for (size_t i = 0; i < prevHyps.size(); ++i) {
-    Costs.data()[i] = prevHyps[i]->GetCost();
-  }
+            mblas::ArrayMatrix& Probs = static_cast<mblas::ArrayMatrix&>(*ProbsEnsemble[0]);
 
-  Probs *= weights[scorers[0]->GetName()];
-  AddBiasVector<byColumn>(Probs, Costs);
+            mblas::ArrayMatrix Costs(Probs.rows(), 1);
+            for (size_t i = 0; i < prevHyps.size(); ++i) {
+                Costs.data()[i] = prevHyps[i]->GetCost();
+            }
 
-  for (size_t i = 1; i < ProbsEnsemble.size(); ++i) {
-	  mblas::ArrayMatrix &currProb = static_cast<mblas::ArrayMatrix&>(*ProbsEnsemble[i]);
+            Probs *= weights[scorers[0]->GetName()];
+            AddBiasVector<byColumn>(Probs, Costs);
 
-	  Probs += weights[scorers[i]->GetName()] * currProb;
-  }
+            for (size_t i = 1; i < ProbsEnsemble.size(); ++i) {
+                mblas::ArrayMatrix &currProb = static_cast<mblas::ArrayMatrix&>(*ProbsEnsemble[i]);
 
-  size_t size = Probs.rows() * Probs.columns(); // Probs.size();
-  std::vector<size_t> keys(size);
-  for (size_t i = 0; i < keys.size(); ++i) {
-    keys[i] = i;
-  }
+                Probs += weights[scorers[i]->GetName()] * currProb;
+            }
 
-  std::vector<size_t> bestKeys(beamSize);
-  std::vector<float> bestCosts(beamSize);
+            size_t size = Probs.rows() * Probs.columns(); // Probs.size();
+            std::vector<size_t> keys(size);
+            for (size_t i = 0; i < keys.size(); ++i) {
+                keys[i] = i;
+            }
 
-  if (!God::Get<bool>("allow-unk")) {
-    blaze::column(Probs, UNK) = std::numeric_limits<float>::lowest();
-  }
+            std::vector<size_t> bestKeys(beamSize);
+            std::vector<float> bestCosts(beamSize);
 
-  std::nth_element(keys.begin(), keys.begin() + beamSize, keys.end(),
-		           ProbCompare(Probs.data()));
+            if (!God::Get<bool>("allow-unk")) {
+                blaze::column(Probs, UNK) = std::numeric_limits<float>::lowest();
+            }
 
-  for (int i = 0; i < beamSize; ++i) {
-    bestKeys[i] = keys[i];
-    bestCosts[i] = Probs.data()[keys[i]];
-  }
+            std::nth_element(keys.begin(), keys.begin() + beamSize, keys.end(),
+                             ProbCompare(Probs.data()));
 
-  std::vector<HostVector<float>> breakDowns;
-  bool doBreakdown = God::Get<bool>("n-best");
-  if (doBreakdown) {
-    breakDowns.push_back(bestCosts);
-    for (size_t i = 1; i < ProbsEnsemble.size(); ++i) {
-      HostVector<float> modelCosts(beamSize);
-      mblas::ArrayMatrix &currProb = static_cast<mblas::ArrayMatrix&>(*ProbsEnsemble[i]);
+            for (int i = 0; i < beamSize; ++i) {
+                bestKeys[i] = keys[i];
+                bestCosts[i] = Probs.data()[keys[i]];
+            }
 
-      auto it = boost::make_permutation_iterator(currProb.begin(), keys.begin());
-      std::copy(it, it + beamSize, modelCosts.begin());
-      breakDowns.push_back(modelCosts);
-    }
-  }
+            std::vector<HostVector<float>> breakDowns;
+            bool doBreakdown = God::Get<bool>("n-best");
+            if (doBreakdown) {
+                breakDowns.push_back(bestCosts);
+                for (size_t i = 1; i < ProbsEnsemble.size(); ++i) {
+                    HostVector<float> modelCosts(beamSize);
+                    mblas::ArrayMatrix &currProb = static_cast<mblas::ArrayMatrix&>(*ProbsEnsemble[i]);
 
-  bool filter = God::Get<std::vector<std::string>>("softmax-filter").size();
+                    auto it = boost::make_permutation_iterator(currProb.begin(), keys.begin());
+                    std::copy(it, it + beamSize, modelCosts.begin());
+                    breakDowns.push_back(modelCosts);
+                }
+            }
 
-  for (size_t i = 0; i < beamSize; i++) {
-    size_t wordIndex = bestKeys[i] % Probs.columns();
+            bool filter = God::Get<std::vector<std::string>>("softmax-filter").size();
 
-    if (filter) {
-      wordIndex = filterIndices[wordIndex];
-    }
+            for (size_t i = 0; i < beamSize; i++) {
+                size_t wordIndex = bestKeys[i] % Probs.columns();
 
-    size_t hypIndex  = bestKeys[i] / Probs.columns();
-    float cost = bestCosts[i];
+                if (filter) {
+                    wordIndex = filterIndices[wordIndex];
+                }
 
-    HypothesisPtr hyp;
-    if (returnAlignment) {
-      std::vector<SoftAlignmentPtr> alignments;
-      for (auto& scorer : scorers) {
-        if (CPU::EncoderDecoder* encdec = dynamic_cast<CPU::EncoderDecoder*>(scorer.get())) {
-          auto& attention = encdec->GetAttention();
-          size_t attLength = attention.columns();
-          alignments.emplace_back(new SoftAlignment(attention.begin(hypIndex), attention.end(hypIndex)));
-        } else {
-          UTIL_THROW2("Return Alignment is allowed only with Nematus scorer.");
+                size_t hypIndex  = bestKeys[i] / Probs.columns();
+                float cost = bestCosts[i];
+
+                // print scores for histogram
+                // out_hist << i << "\t" << hypIndex << "\t\t" << bestCosts[i] << " - " << wordIndex <<  " = " << God::GetTargetVocab().operator[](wordIndex) <<  endl;
+                out_hist << wordIndex <<  "\t" << God::GetTargetVocab().operator[](wordIndex) << "\t" << bestCosts[i] << endl;
+
+
+                HypothesisPtr hyp;
+                if (returnAlignment) {
+                    std::vector<SoftAlignmentPtr> alignments;
+                    for (auto& scorer : scorers) {
+                        if (CPU::EncoderDecoder* encdec = dynamic_cast<CPU::EncoderDecoder*>(scorer.get())) {
+                            auto& attention = encdec->GetAttention();
+                            size_t attLength = attention.columns();
+                            alignments.emplace_back(new SoftAlignment(attention.begin(hypIndex), attention.end(hypIndex)));
+                        } else {
+                            UTIL_THROW2("Return Alignment is allowed only with Nematus scorer.");
+                        }
+                    }
+
+                    hyp.reset(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost, alignments));
+                } else {
+                    hyp.reset(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost));
+                }
+
+                if (doBreakdown) {
+                    hyp->GetCostBreakdown().resize(ProbsEnsemble.size());
+                    float sum = 0;
+                    for(size_t j = 0; j < ProbsEnsemble.size(); ++j) {
+                        if (j == 0) {
+                            hyp->GetCostBreakdown()[0] = breakDowns[0][i];
+                        } else {
+                            float cost = 0;
+                            if (j < ProbsEnsemble.size()) {
+                                if (prevHyps[hypIndex]->GetCostBreakdown().size() < ProbsEnsemble.size())
+                                    const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown().resize(ProbsEnsemble.size(), 0.0);
+                                cost = breakDowns[j][i] + const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown()[j];
+                            }
+                            sum += weights[scorers[j]->GetName()] * cost;
+                            hyp->GetCostBreakdown()[j] = cost;
+                        }
+                    }
+                    hyp->GetCostBreakdown()[0] -= sum;
+                    hyp->GetCostBreakdown()[0] /= weights[scorers[0]->GetName()];
+                }
+                bestHyps.push_back(hyp);
+            }
+
+
+            out_hist.close();
         }
-      }
 
-      hyp.reset(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost, alignments));
-    } else {
-      hyp.reset(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost));
+
     }
-
-    if (doBreakdown) {
-      hyp->GetCostBreakdown().resize(ProbsEnsemble.size());
-      float sum = 0;
-      for(size_t j = 0; j < ProbsEnsemble.size(); ++j) {
-      if (j == 0) {
-        hyp->GetCostBreakdown()[0] = breakDowns[0][i];
-      } else {
-        float cost = 0;
-        if (j < ProbsEnsemble.size()) {
-          if (prevHyps[hypIndex]->GetCostBreakdown().size() < ProbsEnsemble.size())
-            const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown().resize(ProbsEnsemble.size(), 0.0);
-          cost = breakDowns[j][i] + const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown()[j];
-        }
-        sum += weights[scorers[j]->GetName()] * cost;
-        hyp->GetCostBreakdown()[j] = cost;
-      }
-      }
-      hyp->GetCostBreakdown()[0] -= sum;
-      hyp->GetCostBreakdown()[0] /= weights[scorers[0]->GetName()];
-    }
-    bestHyps.push_back(hyp);
-  }
-}
-
-
-}
 }
 
